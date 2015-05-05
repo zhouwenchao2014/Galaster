be9d7f8753b0aaa5992cb55910fc493e9f4c2b2e
Kontinuation <morisama@yeah.net>
2015-05-05 20:51:43 +0800

    keep floating point representation of RGB components in color_type

--- prop.hh
+++ prop.hh
@@ -24,13 +24,21 @@ enum class stroke_type {
 class color_type {
 public:
     color_type(uint8_t r, uint8_t g, uint8_t b)
+        : r(r * recp_255), g(g * recp_255), b(b * recp_255) {
+    }
+    color_type(int r, int g, int b)
+        : r(r * recp_255), g(g * recp_255), b(b * recp_255) {
+    }
+    color_type(double r, double g, double b)
         : r(r), g(g), b(b) {
     }
-    uint8_t r, g, b;
 
-    double redd(void) const { return r / 255.0; }
-    double greend(void) const { return g / 255.0; }
-    double blued(void) const { return b / 255.0; }
+    double r, g, b;
+    constexpr static const double recp_255 = 1.0 / 255.0;
+
+    double redd(void) const { return r; }
+    double greend(void) const { return g; }
+    double blued(void) const { return b; }
 
     static const color_type red, green, blue, white;
 };
