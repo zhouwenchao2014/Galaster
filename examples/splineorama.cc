9e6144cbe5447c86ef1305fa4cef7a8d0d6f96a0
Kontinuation <morisama@yeah.net>
2015-05-24 04:18:55 +0800

    refined splineorama example

--- examples/splineorama.cc
+++ examples/splineorama.cc
@@ -40,8 +40,10 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     if (!g_graph) return;
     if (key == 'A' and action == GLFW_RELEASE) {
         new std::thread([&]() {
+                std::vector<vertex_styled<_float_type> *> vs;
                 graph_type *graph = the_graph;
                 _float_type r = 5;
+
                 for (int k = 0; k < n_vertex; k++) {
                     auto v = new vertex_styled<_float_type>(
                         rand_range(-r, r),
@@ -53,20 +55,22 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
                     v->size = 7;
                     v->shape_detail = 30;
                     graph->add_vertex(v);
+                    vs.push_back(v);
 
                     auto e = new edge_styled<_float_type>(centroid, v);
-                    e->set_spline();
+                    // e->set_spline();
+                    e->strength = 0.1;
                     e->color = color_type(0.3, 0.3, 0.3);
                     graph->add_edge(e);
                     usleep(10000);
                 }
 
-                for (int i = 1; i < n_vertex; i++) {
+                for (int i = 0; i < n_vertex; i++) {
                     for (int j = i + 1; j < n_vertex; j++) {
                         if (rand_range(0,1) < 4.0 / n_vertex) {
                             auto e = new edge_styled<_float_type>(
-                                graph->g->vs[i],
-                                graph->g->vs[j]);
+                                vs[i],
+                                vs[j]);
                             e->stroke = stroke_type::dashed;
                             e->set_spline();
                             graph->add_edge(e);
@@ -97,9 +101,9 @@ int main(void)
 {
     GLFWwindow *window = galaster_init();
     if (window) {
-        the_graph = generate_splineorama_graph(6);
+        the_graph = generate_splineorama_graph(1);
         glfwSetKeyCallback(window, key_callback);
-        galaster_run(window, the_graph, 0.3);
+        galaster_run(window, the_graph, 3.0);
     }
     delete the_graph;
     return 0;
